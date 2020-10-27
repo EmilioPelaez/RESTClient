57701ffac9a60158deb1d132a1ace69c5e5aac9b
Emilio Peláez <me@epelaez.net>
2020-10-27 15:02:08 +0100

    Create project

---
+++ RESTClient/RESTClient.h
@@ -0,0 +1,18 @@
+//
+//  RESTClient.h
+//  RESTClient
+//
+//  Created by Emilio Peláez on 27/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RESTClient.
+FOUNDATION_EXPORT double RESTClientVersionNumber;
+
+//! Project version string for RESTClient.
+FOUNDATION_EXPORT const unsigned char RESTClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RESTClient/PublicHeader.h>
+
+
