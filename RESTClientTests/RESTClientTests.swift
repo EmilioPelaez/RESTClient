01906f2d856691cf29b889d657b2f5660547b6ec
Emilio Peláez <me@epelaez.net>
2020-10-28 17:06:36 +0100

    Final test changes

--- RESTClientTests/RESTClientTests.swift
+++ RESTClientTests/RESTClientTests.swift
@@ -6,17 +6,19 @@
 //
 
 import XCTest
+import Combine
 @testable import RESTClient
 
 class RESTClientTests: XCTestCase {
 	
 	let values = (1...10)
-	let client = RESTClient(baseUrl: URL(string: "localhost:8080")!)
+	let client = RESTClient(baseUrl: URL(string: "http://localhost:8080")!)
+	var bag: Set<AnyCancellable> = []
 	
 	func testAll() {
 		let expectation = XCTestExpectation(description: "All")
 		
-		_ = client.all(Object.self).sink {
+		client.all(Object.self).sink {
 			switch $0 {
 			case .failure(let error):
 				XCTFail("Fetch fail \(error.localizedDescription)")
@@ -25,14 +27,14 @@ class RESTClientTests: XCTestCase {
 		} receiveValue: { objects in
 			XCTAssertEqual(objects, self.values.map { Object(id: $0, value: "Hello") })
 			expectation.fulfill()
-		}
+		}.store(in: &bag)
 		wait(for: [expectation], timeout: 10.0)
 	}
 	
 	func testFirst() {
 		let expectation = XCTestExpectation(description: "First")
 		
-		_ = client.first(Object.self).sink {
+		client.first(Object.self).sink {
 			switch $0 {
 			case .failure(let error):
 				XCTFail("Fetch fail \(error.localizedDescription)")
@@ -41,14 +43,14 @@ class RESTClientTests: XCTestCase {
 		} receiveValue: { object in
 			XCTAssertEqual(object, Object(id: 1, value: "Hello"))
 			expectation.fulfill()
-		}
+		}.store(in: &bag)
 		wait(for: [expectation], timeout: 10.0)
 	}
 	
 	func testFind() {
 		let expectation = XCTestExpectation(description: "Find")
 		
-		_ = client.find(Object.self, identifier: 1).sink {
+		client.find(Object.self, identifier: 1).sink {
 			switch $0 {
 			case .failure(let error):
 				XCTFail("Fetch fail \(error.localizedDescription)")
@@ -57,14 +59,14 @@ class RESTClientTests: XCTestCase {
 		} receiveValue: { object in
 			XCTAssertEqual(object, Object(id: 1, value: "Hello"))
 			expectation.fulfill()
-		}
+		}.store(in: &bag)
 		wait(for: [expectation], timeout: 10.0)
 	}
 	
 	func testCreate() {
 		let expectation = XCTestExpectation(description: "Create")
 		
-		_ = client.create(NewObject(value: "Hello"), receive: Object.self).sink {
+		client.create(NewObject(value: "Hello"), receive: Object.self).sink {
 			switch $0 {
 			case .failure(let error):
 				XCTFail("Fetch fail \(error.localizedDescription)")
@@ -73,14 +75,14 @@ class RESTClientTests: XCTestCase {
 		} receiveValue: { object in
 			XCTAssertEqual(object, Object(id: 11, value: "Hello"))
 			expectation.fulfill()
-		}
+		}.store(in: &bag)
 		wait(for: [expectation], timeout: 10.0)
 	}
 	
 	func testDelete() {
 		let expectation = XCTestExpectation(description: "Delete")
 		
-		_ = client.delete(Object.self, identifier: "1").sink {
+		client.delete(Object.self, identifier: "1").sink {
 			switch $0 {
 			case .failure(let error):
 				XCTFail("Fetch fail \(error.localizedDescription)")
@@ -88,7 +90,7 @@ class RESTClientTests: XCTestCase {
 			}
 		} receiveValue: {
 			expectation.fulfill()
-		}
+		}.store(in: &bag)
 		wait(for: [expectation], timeout: 10.0)
 	}
 	
