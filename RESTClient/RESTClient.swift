c4e5a0e1cb64af10217bdbe3436f0563238a6b1d
Emilio Peláez <me@epelaez.net>
2020-11-18 12:58:12 +0100

    Improve generics names

--- RESTClient/RESTClient.swift
+++ RESTClient/RESTClient.swift
@@ -24,52 +24,52 @@ open class RESTClient: HTTPClient, TopLevelDecoder {
 		super.init(session: session)
 	}
 	
-	open func all<T: Decodable>(_ type: T.Type, router: Router? = nil) -> AnyPublisher<[T], Error> {
+	open func all<Resource: Decodable>(_ type: Resource.Type, router: Router? = nil) -> AnyPublisher<[Resource], Error> {
 		let url = (router ?? self.router).url(for: type, baseURL: baseUrl)
 		return performRequest(for: url, method: .GET, configuration: requestConfiguration)
 			.map(\.data)
-			.decode(type: [T].self, decoder: self)
+			.decode(type: [Resource].self, decoder: self)
 			.eraseToAnyPublisher()
 	}
 	
-	open func first<T: Decodable>(_ type: T.Type, router: Router? = nil) -> AnyPublisher<T?, Error> {
+	open func first<Resource: Decodable>(_ type: Resource.Type, router: Router? = nil) -> AnyPublisher<Resource?, Error> {
 		let url = (router ?? self.router).url(for: type, baseURL: baseUrl)
 		return performRequest(for: url, method: .GET, configuration: requestConfiguration)
 			.map(\.data)
-			.decode(type: [T].self, decoder: self)
+			.decode(type: [Resource].self, decoder: self)
 			.map(\.first)
 			.eraseToAnyPublisher()
 	}
 	
-	open func find<T: UniqueRemoteResource>(_ type: T.Type, identifier: T.ID, router: Router? = nil) -> AnyPublisher<T, Error> {
+	open func find<Resource: UniqueRemoteResource>(_ type: Resource.Type, identifier: Resource.ID, router: Router? = nil) -> AnyPublisher<Resource, Error> {
 		let url = (router ?? self.router).url(for: type, identifier: identifier, baseURL: baseUrl)
 		return performRequest(for: url, method: .GET, configuration: requestConfiguration)
 			.map(\.data)
-			.decode(type: T.self, decoder: self)
+			.decode(type: Resource.self, decoder: self)
 			.eraseToAnyPublisher()
 	}
 	
-	open func create<T: Encodable, U: Decodable>(_ body: T, receive: U.Type, router: Router? = nil) -> AnyPublisher<U, Error> {
+	open func create<Body: Encodable, Resource: Decodable>(_ body: Body, receive: Resource.Type, router: Router? = nil) -> AnyPublisher<Resource, Error> {
 		let url = (router ?? self.router).url(for: type(of: body), baseURL: baseUrl)
 		return performRequest(for: url, method: .POST, body: try HTTPBody(body, encoder: encoder), configuration: requestConfiguration)
 			.map(\.data)
-			.decode(type: U.self, decoder: self)
+			.decode(type: Resource.self, decoder: self)
 			.eraseToAnyPublisher()
 	}
 	
-	open func update<T: UniqueRemoteResource>(_ resource: T, router: Router? = nil) -> AnyPublisher<T, Error> {
+	open func update<Resource: UniqueRemoteResource>(_ resource: Resource, router: Router? = nil) -> AnyPublisher<Resource, Error> {
 		let url = (router ?? self.router).url(for: resource, baseURL: baseUrl)
 		return performRequest(for: url, method: .PUT, body: try HTTPBody(resource, encoder: encoder), configuration: requestConfiguration)
 			.map(\.data)
-			.decode(type: T.self, decoder: self)
+			.decode(type: Resource.self, decoder: self)
 			.eraseToAnyPublisher()
 	}
 	
-	open func delete<T: UniqueRemoteResource>(_ resource: T, router: Router? = nil) -> AnyPublisher<Void, Error> {
+	open func delete<Resource: UniqueRemoteResource>(_ resource: Resource, router: Router? = nil) -> AnyPublisher<Void, Error> {
 		delete(type(of: resource), identifier: resource.id, router: router)
 	}
 	
-	open func delete<T: UniqueRemoteResource>(_ type: T.Type, identifier: T.ID, router: Router? = nil) -> AnyPublisher<(), Error> {
+	open func delete<Resource: UniqueRemoteResource>(_ type: Resource.Type, identifier: Resource.ID, router: Router? = nil) -> AnyPublisher<(), Error> {
 		let url = (router ?? self.router).url(for: type, identifier: identifier, baseURL: baseUrl)
 		return performRequest(for: url, method: .DELETE, configuration: requestConfiguration)
 			.map { _ in  }
