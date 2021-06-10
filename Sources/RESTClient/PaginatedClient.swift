1c33a257135dd13a04c046e306b997053be18db4
Emilio Peláez <me@epelaez.net>
2021-06-10 09:38:10 +0200

    Paginated Client async await methods

--- Sources/RESTClient/PaginatedClient.swift
+++ Sources/RESTClient/PaginatedClient.swift
@@ -23,22 +23,34 @@ open class PaginatedClient<Page: Decodable>: ResourceClient {
 		super.init(baseUrl: baseUrl, router: router, updateMethod: updateMethod, session: session, encoder: encoder, decoder: decoder)
 	}
 	
-	open func page<Resource: Decodable>(_ type: Resource.Type, router: Router? = nil, page: Int, pageSize: Int) -> AnyPublisher<PaginatedResponse<Page, [Resource]>, Error> {
+	open func buildUrl<Resource: Decodable>(_ type: Resource.Type, router: Router? = nil, page: Int, pageSize: Int) throws -> URL {
 		let url = (router ?? self.router).url(for: type, baseURL: baseUrl)
-		guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
-			return Fail(error: PaginatedClientError.invalidUrl).eraseToAnyPublisher()
-		}
+		guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { throw PaginatedClientError.invalidUrl }
 		components.queryItems = [
 			URLQueryItem(name: pageKey, value: "\(page)"),
 			URLQueryItem(name: pageSizeKey, value: "\(pageSize)")
 		]
-		guard let finalUrl = components.url else {
+		guard let finalUrl = components.url else { throw PaginatedClientError.invalidUrl }
+		return finalUrl
+	}
+	
+	@available(iOS 15.0, *)
+	open func page<Resource: Decodable>(_ type: Resource.Type, router: Router? = nil, page: Int, pageSize: Int) async throws -> PaginatedResponse<Page, [Resource]> {
+		let url = try buildUrl(type, router: router, page: page, pageSize: pageSize)
+		let result = try await performRequest(for: url, method: .GET, configuration: requestConfiguration)
+		return try decode(PaginatedResponse<Page, [Resource]>.self, from: result.0)
+	}
+	
+	open func page<Resource: Decodable>(_ type: Resource.Type, router: Router? = nil, page: Int, pageSize: Int) -> AnyPublisher<PaginatedResponse<Page, [Resource]>, Error> {
+		do {
+			let url = try buildUrl(type, router: router, page: page, pageSize: pageSize)
+			return performRequest(for: url, method: .GET, configuration: requestConfiguration)
+				.map(\.data)
+				.decode(type: PaginatedResponse<Page, [Resource]>.self, decoder: self)
+				.eraseToAnyPublisher()
+		} catch {
 			return Fail(error: PaginatedClientError.invalidUrl).eraseToAnyPublisher()
 		}
-		return performRequest(for: finalUrl, method: .GET, configuration: requestConfiguration)
-			.map(\.data)
-			.decode(type: PaginatedResponse<Page, [Resource]>.self, decoder: self)
-			.eraseToAnyPublisher()
 	}
 	
 }
